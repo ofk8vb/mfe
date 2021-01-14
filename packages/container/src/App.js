3ad2024875cfbef673381703ba70c1dca8011bdc
ofk8vb <latios_gs@hotmail.com>
2021-01-14 16:22:10 +0300

    randomized css name generation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 // This is the main app ( container app itself )
 // mount functions display the marketingApp at the specified(passed in as argument) html element
 
-
-export default()=>{
-    return(
-        <BrowserRouter>
-             <div>
-                <Header/>
-                {/* // the ref.current argument passed into the mount function back at MarketingApp.js */}
-                {/* // gets the parent <div></div> element */}
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
-       
-    ) 
-}
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+export default () => {
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          {/* // the ref.current argument passed into the mount function back at MarketingApp.js */}
+          {/* // gets the parent <div></div> element */}
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+// Whenever application is build for production, classes will be generated with prefix of ma
+// not jss which could lead to classname collision inside container if someother
+// microfrontend is using the same css in js library
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
 export default () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing}/>
-                    <Route path="/" component={Landing}/>
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
-}
+  );
+};
